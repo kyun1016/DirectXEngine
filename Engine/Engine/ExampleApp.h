8917ee70d1c0651a59cf95a4e409f2b05af49716
kyun1016 <42373004+kyun1016@users.noreply.github.com>
2024-05-28 00:36:20 +0900

    TODO: Make Model Translate Algorithm.

--- Engine/Engine/ExampleApp.h
+++ Engine/Engine/ExampleApp.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "AppBase.h"
+#include "GeometryGenerator.h"
 
 namespace kyun {
 	class ExampleApp : public AppBase
 	{
 	public:
 		ExampleApp();
+
+		virtual bool InitScene() override;
+
 		virtual void UpdateGUI() override;
 		virtual void Update(float dt) override;
 		virtual void Render() override;
